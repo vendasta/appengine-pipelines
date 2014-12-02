7d0b0d930e42128190c9e963ab8021ae41ae59af
Kevin Sookocheff <ksookocheff@vendasta.com>
2014-12-02 10:27:11 -0600

    Remove simplejson dependency

---
+++ python/src/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="pipelines",
+    version="1.0.0",
+    packages=find_packages(),
+    author="VendAsta",
+    author_email="jcollins@vendasta.com",
+    keywords="google app engine pipelines data processing",
+    url="https://github.com/vendasta/appengine-mapreduce.git",
+    license="Apache License 2.0",
+    description="Connects together complex workflows.",
+    include_package_data=True,
+    install_requires=[
+            "simplejson >= 3.6.5",
+        ]
+)
